2fd19b9be72b87b7bd8c7d9930cadc53d64e4832
Reynaldo Abreu <reynaldodadilson@hotmail.com>
2023-07-25 22:14:35 -0300

    configyrando o projeto com o banco postgresql

--- src/test/java/com/example/apicadastro/api/resource/CompanyControllerTest.java
+++ src/test/java/com/example/apicadastro/api/resource/CompanyControllerTest.java
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
-@WebMvcTest
+@WebMvcTest(controllers = CompanyController.class)
 @AutoConfigureMockMvc
 public class CompanyControllerTest {
 
